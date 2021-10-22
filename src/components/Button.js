1eb0c244b70463a5f3b8bfe51a251916a5621e70
David <davemills093@gmail.com>
2021-10-22 15:46:13 -0400

    Added headeer and btn

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react'
+
+
+const Button = ({ color, text }) => {
+    return (
+        <button style={{backgroundColor: color }} className='btn'>
+            {text}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'black'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color:PropTypes.string,
+}
+
+export default Button

ee36200874754c7e23834d9bd547acb3e7e80a81
David <davemills093@gmail.com>
2021-10-24 14:14:13 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
 import Header from './components/Header';
-import Button from './components/Button';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks ] = useState([
+    {
+      id: 1,
+      text: 'Feed the Dogs',
+      day: 'Daily',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Clip Dogs Nails',
+      day: 'bi-weekly',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Grocery Shopping',
+      day: 'Wednesday of Every Week',
+      reminder: false,
+    }
+])
+
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }

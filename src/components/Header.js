eb695a7771745688bb8e8293581500b53d2b3f2c
David <davemills093@gmail.com>
2021-10-24 21:06:36 -0400

    setShow task

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-    const onClick = (e) => {
-        console.log('Hello')
-    }
+const Header = ({ title, onAdd }) => {
     
     return (
         <header className='header'>
             <h1> {title} </h1>
             <Button color='green' text='Add' onClick=
-            {onClick} />
+            {onAdd} />
         </header>
     )
 }
